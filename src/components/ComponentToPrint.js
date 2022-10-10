ff877eeaa4732fb88f79fd097684be6843ec48ed
Shaibal Sharif <shaibalsharif@gmail.com>
2022-10-10 18:11:54 +0600

    Need Approval

--- src/components/ComponentToPrint.js
+++ src/components/ComponentToPrint.js
@@ -5,7 +5,7 @@ const ComponentToPrint = React.forwardRef(({ data }, ref) => {
     const DTtoElement = (values) => {
         return (
             <div className="grid grid-rows-2 text-center w-[70%] leading-4
-             bg-slate-300 m-2 px-2 border-red-500 ">
+             bg-slate-300 m-2 px-2 bg-opacity-75 border-red-500 ">
                 <p className="text-start text-sm ">{values[0]}</p>
                 <h1 className="text-start">{values[1]}</h1>
             </div>
@@ -13,9 +13,10 @@ const ComponentToPrint = React.forwardRef(({ data }, ref) => {
     }
 
     return (
-        <div ref={ref}>
+        <div ref={ref} >
             <div className="space-y-4">
                 <Header />
+                <img className="opacity-40 absolute z-[-1] h-[50%]  top-[30%]" src="https://aessk.org.bd/wp-content/uploads/2019/01/cropped-logo-alumni.png"/>
                 <div className="grid grid-cols-2 mx-[15%] ">
                     {Object.entries(data).map((dt) => DTtoElement(dt))}
                 </div>
